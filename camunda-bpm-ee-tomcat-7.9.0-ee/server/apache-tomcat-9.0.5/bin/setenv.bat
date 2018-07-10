c9eb3abd0e557185ce361aea0cf6419da1571bee
raggafra <raggafra@gmail.com>
2018-07-10 14:36:20 +0200

    pulito tutto

--- camunda-bpm-ee-tomcat-7.9.0-ee/server/apache-tomcat-9.0.5/bin/setenv.bat
+++
@@ -1,3 +0,0 @@
-set CATALINA_OPTS=-Xmx512m -XX:MaxPermSize=256m -XX:PermSize=256m
-set "JAVA_HOME="
-set "JRE_HOME=C:\Program Files (x86)\Java\jre1.8.0_171"
--- src/script1.java
+++
@@ -1,7 +0,0 @@
- public class scriptProva implements  JavaDelegate{
-	public Boolean anomalie1;	
-
-	public void execute(){
-		anomalie1=true;
-	}
-}
